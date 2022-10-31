88f55c8ff01d878be0143446605af2c55b203d44
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2022-10-31 15:08:39 -0600

    Add files via upload

---
+++ buffet.py
@@ -0,0 +1,8 @@
+buffet = ("hamburgers", "hotdogs", "french fries", "chicken wings", "nachos")
+for food in buffet:
+        print(food)
+# I have put a comment sign at the beginning of the next line so you can still run the code and have it work. If I were to remove, you would get an error.
+# error line buffet[0] = "salad"
+buffet = ("hamburgers", "hotdogs", "french fries", "breadsticks", "salad")
+for food in buffet:
+        print(food) 
